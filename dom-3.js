c250e4f298c034eb18424e959950da32c31efe35
Amir Hamza <amirhamja7055@gmail.com>
2024-02-03 20:52:58 -0800

    copy item add

--- dom-3.js
+++ dom-3.js
@@ -1,22 +1,37 @@
 // // project requerment
 // * change background color by genarating random rgb color by click a button
 // * Also display the hex code to a diseble input field 
-
+// *add a toast message 
 // step 1- (creat onload handeler create)
+// global var
+let copyDive=null;
+
 window.onload=()=>{
     main();
 }
 function main(){
     const containerPart = document.querySelector("#container");
     const inputColector = document.querySelector("#output");
-    const btnPart = document.querySelector(".btn");
-    btnPart.addEventListener("click",()=>{
+    const changePart = document.querySelector(".btn");
+    const copyPart = document.querySelector(".copy-btn");
+
+    changePart.addEventListener("click",()=>{
         bgcolorhext=randomhexColor()
         containerPart.style.backgroundColor=bgcolorhext;
         inputColector.value=bgcolorhext;
     })
+    copyPart.addEventListener("click",function(){
+       window.navigator.clipboard.writeText(inputColector.value)
+        if(copyDive!==null){
+            copyDive.remove()
+            copyDive=null;
+        }
+
+       gettoastmessege(`${inputColector.value} is copied`);
+    })
  
 }
+
 
 
 
@@ -26,6 +41,23 @@ function randomhexColor(){
     let green= Math.floor(Math.random()*255);
     let blue= Math.floor(Math.random()*255);
     return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
+}
+
+const gettoastmessege=(masg)=>{
+    copyDive=document.createElement('div')
+    copyDive.innerHTML=masg;
+    copyDive.className="toast-message toast-message-slide-in"
+
+    copyDive.addEventListener('click',function(){
+        copyDive.classList.remove("toast-message-slide-in");
+        copyDive.classList.add("toast-message-slide-out")
+        copyDive.addEventListener("animationend",function(){
+            copyDive.remove()
+            copyDive=null;
+        })
+    })
+    document.body.appendChild(copyDive)
+
 }
 
 // step - 3( collect all necessary referanse)
